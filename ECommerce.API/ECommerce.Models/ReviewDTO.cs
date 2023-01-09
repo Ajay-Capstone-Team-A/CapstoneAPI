7ef03101cd1ca337f3c9775eede331c6e216204c
Calvin Tan <calvintan607@gmail.com>
2023-01-09 12:47:16 -0800

    reviewId added to reviewDTO, reviews now load sorted by reviewID

--- ECommerce.API/ECommerce.Models/ReviewDTO.cs
+++ ECommerce.API/ECommerce.Models/ReviewDTO.cs
@@ -8,15 +8,25 @@ namespace ECommerce.Models
 {
     public class ReviewDTO
     {
+        public int ReviewId { get; set; }
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
-
         public int ProductId { get; set; }
         public string Comment { get; set; }
         public int Rating { get; set; }
 
         public ReviewDTO(string UserFirstName, string UserLastName, int ProductId, string Comment, int Rating)
         {
+            this.UserFirstName = UserFirstName;
+            this.UserLastName = UserLastName;
+            this.ProductId = ProductId;
+            this.Comment = Comment;
+            this.Rating = Rating;
+        }
+
+        public ReviewDTO(int ReviewId,string UserFirstName, string UserLastName, int ProductId, string Comment, int Rating)
+        {
+            this.ReviewId = ReviewId;
             this.UserFirstName = UserFirstName;
             this.UserLastName = UserLastName;
             this.ProductId = ProductId;
